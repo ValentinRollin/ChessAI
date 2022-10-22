607c5a19d4880358dc55f9503390be14a806f212
ValentinRollin <valentinrollin@outlook.fr>
2022-10-22 16:13:30 +0200

    creation fenetre

--- chess_game/constants.py
+++ chess_game/constants.py
@@ -1,9 +1,9 @@
 import pygame
 import os
 
-largeur, longueur = 760,70
+largeur, longueur = 760,760
 ligne, colonne = 8,8
-carree = largeur // longueur
+case = largeur/12
 
 
 marron = (87,16,16)
@@ -13,18 +13,17 @@ Path = "chess_game/chess_images"
 
 #black peieces
 
-noir_cav = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bKN.png")),(carree, carree))
-noir_fou = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bB.png")),(carree, carree))
-noir_roi = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bK.png")),(carree, carree))
-noir_pion = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bP.png")),(carree, carree))
-noir_reine = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bQ.png")),(carree, carree))
-noir_tour = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bR.png")),(carree, carree))
-
+noir_cav = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bKN.png")),(case, case))
+noir_fou = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bB.png")),(case, case))
+noir_roi = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bK.png")),(case, case))
+noir_pion = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bP.png")),(case, case))
+noir_reine = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bQ.png")),(case, case))
+noir_tour = pygame.transform.scale(pygame.image.load(os.path.join(Path,"bR.png")),(case, case))
 #white pieces 
 
-blanc_cav = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wKN.png")),(carree, carree))
-blanc_fou = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wB.png")),(carree, carree))
-blanc_roi = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wK.png")),(carree, carree))
-banc_pion = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wp.png")),(carree, carree))
-blanc_reine = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wQ.png")),(carree, carree))
-blanc_tour = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wR.png")),(carree, carree))
+blanc_cav = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wKN.png")),(case, case))
+blanc_fou = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wB.png")),(case, case))
+blanc_roi = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wK.png")),(case, case))
+banc_pion = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wp.png")),(case, case))
+blanc_reine = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wQ.png")),(case, case))
+blanc_tour = pygame.transform.scale(pygame.image.load(os.path.join(Path,"wR.png")),(case, case))
--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import pygame
 
 from chess_game.constants import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+win = pygame.display.set_mode((largeur,longueur))
+
+def main():
+        run = True
+        FPS = 60
+        while run:
+            clock.tick(FPS)
+            win.blit(blanc_fou, (50,50))
+
+            pygame.display.update()
+
+            
+# Pour pouvoir fermer la fenetre quand on appuie sur la croix
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    quit()
+
+main()
