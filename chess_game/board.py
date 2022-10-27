dbe1a2e42fa929db651e39e4c9bc5ff66840bde4
ValentinRollin <valentinrollin@outlook.fr>
2022-10-27 22:33:43 +0200

    update board + fix constant

--- chess_game/board.py
+++ chess_game/board.py
@@ -8,14 +8,46 @@ class newBoard:
         self.longueur = longueur
         self.case = case
         self.win = win
-        self.ligne = lignes
+        self.lignes = lignes
         self.colonne = colonnes
         self.Board = []
         self.create_Board()
 
     def create_Board(self):
-        for li in range(self.ligne):
+        for ligne in range(self.lignes):
             self.Board.append([0 for i in range(self.colonnes)])
 
             for col in range(self.colonnes):
-                pass
+                #ligne pions noir
+                if ligne == 1 :
+                    self.Board[ligne][col] = Pawn(self.case,noir_pion, Black, "Pawn", ligne, col)
+
+                #ligne pions blanc
+                if ligne == 6 :
+                    self.Board[ligne][col] = Pawn(self.case, blanc_pion, White, "Pawn", ligne, col)
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
