cb92e2cf2b354559e374e2860a2329e514343a43
ValentinRollin <valentinrollin@outlook.fr>
2022-10-28 15:13:02 +0200

    def board + position piece + mouvement pieces

--- chess_game/board.py
+++ chess_game/board.py
@@ -26,8 +26,72 @@ class newBoard:
                 if ligne == 6 :
                     self.Board[ligne][col] = Pawn(self.case, blanc_pion, White, "Pawn", ligne, col)
 
-            
+                #ligne pieces noir
+                if ligne == 0 : 
+                    # cas de la tour
+                    if col == 0 or col == 7: 
+                        self.Board[ligne][col] == Rook(self.case, noir_tour, Black, "Rook", ligne, col)
 
+                    #cas du cavalier
+                    if col == 1 or col == 6 :
+                        self.Board[ligne][col] == Knight(self.case, noir_cav, Black, "Knight",ligne, col)
+
+                    #cas du fou
+                    if col == 2 or col == 5 :
+                        self.Board[ligne][col] == Bishop(self.case, noir_fou, Black, "Bishop", ligne, col)
+
+                    #cas de la reine
+                    if col == 3 :
+                        self.Board[ligne][col] == Queen(self.case, noir_reine, Black, "Queen", ligne, col)
+
+                    #cas du roi
+                    if col == 4 :
+                        self.Board[ligne][col] == King(self.case, noir_roi, Black, "King", ligne, col)
+                
+                #ligne pieces blanches
+                if ligne == 7 :
+                    # cas de la tour
+                    if col == 0 or col == 7: 
+                        self.Board[ligne][col] == Rook(self.case, blanc_tour, White, "Rook", ligne, col)
+
+                    #cas du cavalier
+                    if col == 1 or col == 6 :
+                        self.Board[ligne][col] == Knight(self.case, blanc_cav, White, "Knight",ligne, col)
+
+                    #cas du fou
+                    if col == 2 or col == 5 :
+                        self.Board[ligne][col] == Bishop(self.case, blanc_fou, White, "Bishop", ligne, col)
+
+                    #cas de la reine
+                    if col == 3 :
+                        self.Board[ligne][col] == Queen(self.case, blanc_reine, White, "Queen", ligne, col)
+
+                    #cas du roi
+                    if col == 4 :
+                        self.Board[ligne][col] == King(self.case, blanc_roi, White, "King", ligne, col)                
+                        
+
+    def get_piece(self, ligne, col) :
+        return self.Board[ligne][col]
+
+    def move(self,piece,ligne, col):
+        self.Board[piece.ligne][piece.col], self.Board[ligne][col] = self.Board[ligne, col], self.Board[piece.ligne][piece.col]
+
+        piece.piece_move(ligne, col)
+
+        if piece.type == "Pawn":
+            if piece.first_move : 
+                piece.first_move = False
+
+    def draw_Board(self):
+        self.win.fill(marron)
+
+        for ligne in range (self.lignes) :
+            for col in range(ligne%2, colonnes, 2):
+                pygame.draw.react(self.win, blanc, (case*(ligne), case*(col), case, case))
+
+    def draw_piece(self, piece, Win):
+        Win.blit(piece.image, (piece.x, piece.y))
 
 
 
