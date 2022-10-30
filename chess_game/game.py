3eb3f0f7024065ec38a5427ff8b5761b17d1fd40
ValentinRollin <valentinrollin@outlook.fr>
2022-10-30 15:57:19 +0100

    methodes check + checkmate + simulate_moves

--- chess_game/game.py
+++ chess_game/game.py
@@ -77,3 +77,80 @@ class Game :
             for pos in self.valide_moves :
                 ligne, col = pos[0],pos[1]
                 pygame.draw.circle(self.Win, Green, (col*self.case + self.case//2), self.case//8)
+
+    def check_game(self):
+        if self.checkmate(self.Board):
+            if self.turn == White:
+                print("Black wins")
+                return True
+            else: 
+                print("White wins")
+                return True
+    
+    def enemies_moves (self, piece, Board):
+        ennemies_moves = []
+        for r in range(len(Board)) :
+            for c in range(len(Board[r])):
+                if Board[r][c] != 0 :
+                    if Board[r][c].color != piece.color :
+                        moves = Board[r][c].get_available_moves(r,c,Board)
+                        for move in moves :
+                            ennemies_moves.append(move)
+        return ennemies_moves
+
+    def get_King_pos(self, Board):
+        for r in range(len(Board)):
+            for c in range(len(Board)):
+                if Board[r][c] != 0:
+                    if Board[r][c].type == "King" and Board[r][c].color == self.turn:
+                        return (r,c)
+        
+
+    def simulate_moves(self, piece, ligne, col):
+        piece_ligne, piece_col = piece.ligne, piece.col
+        save_piece = self.Board.Board[ligne][col]
+
+        if self.Board.Board[ligne][col] != 0:
+            self.Board.Board[ligne][col] = 0
+
+        self.Board.Board[piece.ligne][piece.col], self.Board.Board[ligne][col] = self.Board.Board[ligne][col], self.Board.Board[piece.ligne][piece.col]
+
+        king_pos = self.get_King_pos(self.Board.Board)
+        if king_pos in self.enemies_moves(piece, self.Board.Board):
+            piece.ligne, piece.col = piece_ligne, piece_col 
+            self.Board[piece_ligne][piece_col] = piece
+            self.Board.Board[ligne][col] = save_piece 
+            return False
+        
+        piece.ligne, piece.col = piece_ligne, piece_col
+        self.Board.Board[piece_ligne][piece_col] = piece
+        self.Board.Board[ligne][col] = save_piece
+        return True
+
+    def possible_moves(self, Board):
+        possible_moves = []
+        for r in range(len(Board)):
+            for c in range (len(Board[r])):
+                if Board[r][c] != 0:
+                    if Board[r][c].color == self.turn and Board[r][c].type != "King":
+                        moves = Board[r][c].get_availables_moves(r,c,Board)
+
+                        for move in moves :
+                            possible_moves.append(move)
+
+        return possible_moves
+
+    def checkmate(self,Board):
+        king_pos = self.get_King_pos(Board.Board)
+        get_king = Board.get_piece(king_pos[0], king_pos[1])
+
+        king_available_moves = set(get_king.availables_moves(king_pos[0],king_pos[1],Board.Board))
+        enemies_moves_set = set(self.enemies_moves(get_king, Board.Board))
+        king_moves = king_available_moves - enemies_moves_set
+        set1 = king_available_moves.intersection(enemies_moves_set)
+        possible_moves_to_def = set1.intersection(self.possible_moves(Board.Board))
+
+        if len(king_moves)==0 and len(king_available_moves) != 0 and possible_moves_to_def == 0:
+            return True
+
+        return False
